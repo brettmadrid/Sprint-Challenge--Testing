caa6251a8419c1645f77418844338ba9dbc3d996
brettmadrid <brettmadrid9@gmail.com>
2019-04-21 19:30:04 -0700

    tested for a return of an empty array if there are not any games

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,12 +1,13 @@
 const request = require("supertest");
 const server = require("./server.js");
+const db = require('../data/dbConfig.js');
 
-describe('The server', () => {
-  it('should set the testing environment', () => {
+describe("The server", () => {
+  it("should set the testing environment", () => {
     expect(process.env.DB_ENV).toBe("testing");
-  })
+  });
 
-  describe('GET /', () => {
+  describe("GET /", () => {
     it("should return 200", async () => {
       const res = await request(server).get("/");
       expect(res.status).toBe(200);
@@ -14,23 +15,41 @@ describe('The server', () => {
 
     it("should return JSON", async () => {
       const res = await request(server).get("/");
-      expect(res.type).toBe('application/json');
+      expect(res.type).toBe("application/json");
     });
 
     it(`should return { message: 'get successful' }`, async () => {
-      const res = await request(server).get('/');
+      const res = await request(server).get("/");
 
-      expect(res.body).toEqual({ message: 'get successful' });
+      expect(res.body).toEqual({ message: "get successful" });
     });
   });
 
-  describe('/Games testing', () => {
-    it('should respond with an empty array when there are not any games', async () => {
+  describe("/Games testing", () => {
+
+    beforeEach(() => {
+      return db('games').truncate();
+    });
+
+    it("should respond with an empty array when there are not any games", async () => {
+      const res = await request(server).get("/games");
+
+      expect(res.status).toBe(200);
+      expect(res.type).toBe("application/json");
+      expect(res.body).toEqual([]);
+    });
+
+    it('should respond with an array of all the games in the db and a status of 200', async () => {
+      await db('games').insert({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+
       const res = await request(server).get('/games');
+      const data = res.body
 
       expect(res.status).toBe(200);
       expect(res.type).toBe('application/json');
-      expect(res.body).toEqual([]);
+      expect(data.length).toEqual(1);
+      expect(data[0].id).toBe(1);
+      expect(data[0].title).toBe('Pacman');
     });
   });
 });
