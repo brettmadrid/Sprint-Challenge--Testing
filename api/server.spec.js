6532f348652bd7e3bfdfd7a2b6e88af6b0d6fd2b
brettmadrid <brettmadrid9@gmail.com>
2019-04-21 18:58:50 -0700

    tested / endpoint to check for a status response of 200

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,5 +5,12 @@ describe('The server', () => {
   it('should set the testing environment', () => {
     expect(process.env.DB_ENV).toBe("testing");
   })
+
+  describe('GET /', () => {
+    it("should return 200", async () => {
+      const res = await request(server).get("/");
+      expect(res.status).toBe(200);
+    });
+  })
   
 })
