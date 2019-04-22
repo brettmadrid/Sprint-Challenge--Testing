10228b72c03246e1b68aae3e42fb6e727112ac93
brettmadrid <brettmadrid9@gmail.com>
2019-04-21 20:16:45 -0700

    tested getAll function

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -26,4 +26,26 @@ describe('The games model', () => {
     })
   })
 
+  describe('The getAll function', () => {
+
+    it('should retrieve all games from the database', async () => {
+      db('games').insert({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+
+      const allGames = await games.getAll();
+
+      expect(allGames.length).toBe(1);
+    });
+
+    it('should retrieve all required feilds from each game in the database', async () => {
+      db('games').insert({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+
+      const allGames = await games.getAll();
+
+      expect(allGames[0].title).toBe('Pacman');
+      expect(allGames[0].genre).toBe('Arcade');
+      expect(allGames[0].releaseYear).toBe(1980);
+    });
+  })
+
+
 })
