76b4702dad6ee9b7fb1b2bac949fee3a7661b7af
brettmadrid <brettmadrid9@gmail.com>
2019-04-21 18:39:48 -0700

    added / and /games endpoints in server.js

--- api/server.js
+++ api/server.js
@@ -5,3 +5,15 @@ const hobbits = require('../games/gamesModel.js');
 const server = express();
 
 server.use(express.json());
+
+server.get('/', async (req, res) => {
+  res.status(200).json({ message: 'get successful' });
+});
+
+server.get('/games', async (req, res) => {
+  const rows = await games.getAll();
+
+  res.status(200).json(rows);
+});
+
+module.exports = server;
