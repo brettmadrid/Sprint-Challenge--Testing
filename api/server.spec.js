8e222a9d4ffd3592c7b1cdf3ef104e5c349cdc43
brettmadrid <brettmadrid9@gmail.com>
2019-04-21 19:00:55 -0700

    tested for a json to be returned for the / endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -11,6 +11,11 @@ describe('The server', () => {
       const res = await request(server).get("/");
       expect(res.status).toBe(200);
     });
-  })
+
+    it("should return JSON", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toBe('application/json');
+    });
+  });
   
-})
+});
